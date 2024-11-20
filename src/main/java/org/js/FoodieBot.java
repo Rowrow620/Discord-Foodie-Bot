c214dcca44eaeaf6ec97f90094c715e23a879ce1
Rowrow620 <fender620@gmail.com>
2024-11-20 04:04:13 -0800

    fixed secrets

--- src/main/java/org/js/FoodieBot.java
+++ src/main/java/org/js/FoodieBot.java
@@ -1,16 +1,35 @@
 package org.js;
-import net.dv8tion.jda.api.JDABuilder;
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+
 import javax.security.auth.login.LoginException;
 
 public class FoodieBot {
+    private final ShardManager shardManager;
+    private final Dotenv config;
 
-    public FoodieBot(){
+    public FoodieBot() {
 
+        config =  Dotenv.configure().load();
+        String TOKEN = config.get("TOKEN");
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(TOKEN);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.playing("munchies mondays"));
+        shardManager = builder.build();
     }
-    public static void main(String[] args) throws LoginException {
-        final String TOKEN = "MTMwODczODc3NTk1MzcwMjk3Mg.G7Zx2L.dVwRFtMiNNnpAHijfyi00drbOo_hp3kTj7WHfM";
-        JDABuilder jdaBuilder = JDABuilder.createDefault(TOKEN);
 
-        jdaBuilder.build();
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+
+    public Dotenv getConfig() {
+        return config;
+    }
+
+    public static void main(String[] args) throws LoginException {
+        FoodieBot bot = new FoodieBot();
     }
 }
